cecca9acbcceb39fd5392d230f7cafa2e6da51d6
gianmerlino <gianmerlino@978b676e-e148-0410-abc8-4378e866053e>
2008-03-22 21:57:24 +0000

    better directory handling, also actor page stuff

--- trunk/lib/Stasis/ActorPage.pm
+++ trunk/lib/Stasis/ActorPage.pm
@@ -71,15 +71,52 @@ sub page {
     # TOTAL DAMAGE DONE #
     #####################
     
+    # Total up the per-spell per-target keys for each spell (on the side).
+    my %damage_spell;
+    my %damage_target;
+    
     my $alldmg = 0;
-    if( $self->{ext}{Damage}{actors}{$PLAYER} ) {
-        foreach my $spell (keys %{$self->{ext}{Damage}{actors}{$PLAYER}}) {
-            foreach my $target (keys %{$self->{ext}{Damage}{actors}{$PLAYER}{$spell}}) {
-                # Skip friendlies
-                next if $self->{raid}{$target}{class};
-
-                # Add the damage.
-                $alldmg += $self->{ext}{Damage}{actors}{$PLAYER}{$spell}{$target}{total};
+    my $dmg_to_mobs = 0;
+    foreach my $dmg_actor ( keys %{$self->{ext}{Damage}{actors}} ) {
+        # Look at the $PLAYER and pets.
+        next unless $dmg_actor eq $PLAYER || ( exists $self->{raid}{$PLAYER} && grep $_ eq $dmg_actor, @{$self->{raid}{$PLAYER}{pets}} );
+        
+        foreach my $spell (keys %{$self->{ext}{Damage}{actors}{$dmg_actor}}) {;
+            # Encode pet damage like this.
+            my $displayspell = $dmg_actor eq $PLAYER ? $spell : "$dmg_actor: $spell"; 
+            
+            foreach my $target (keys %{$self->{ext}{Damage}{actors}{$dmg_actor}{$spell}}) {
+                # Include all damage in the per-spell and per-target running totals.
+                foreach my $key (keys %{$self->{ext}{Damage}{actors}{$dmg_actor}{$spell}{$target}}) {
+                    my $keys = $self->{ext}{Damage}{actors}{$dmg_actor}{$spell}{$target};
+                    if( $key =~ /([Mm]in|[Mm]ax)$/ ) {
+                        # TARGET
+                        if( lc $1 eq "min" && (!$damage_target{$target}{$key} || $keys->{$key} < $damage_target{$target}{$key}) ) {
+                            $damage_target{$target}{$key} = $keys->{$key};
+                        } elsif( lc $1 eq "max" && (!$damage_target{$target}{$key} || $keys->{$key} > $damage_target{$target}{$key}) ) {
+                            $damage_target{$target}{$key} = $keys->{$key};
+                        }
+                        
+                        # SPELL
+                        if( lc $1 eq "min" && (!$damage_spell{$displayspell}{$key} || $keys->{$key} < $damage_spell{$displayspell}{$key}) ) {
+                            $damage_spell{$displayspell}{$key} = $keys->{$key};
+                        } elsif( lc $1 eq "max" && (!$damage_spell{$displayspell}{$key} || $keys->{$key} > $damage_spell{$displayspell}{$key}) ) {
+                            $damage_spell{$displayspell}{$key} = $keys->{$key};
+                        }
+                    } else {
+                        # TARGET
+                        $damage_target{$target}{$key} += $keys->{$key};
+                        
+                        # SPELL
+                        $damage_spell{$displayspell}{$key} += $keys->{$key};
+                    }
+                }
+                
+                # Add to total damage.
+                $alldmg += $self->{ext}{Damage}{actors}{$dmg_actor}{$spell}{$target}{total};
+                
+                # Skip friendlies when totaling damage to mobs.
+                $dmg_to_mobs += $self->{ext}{Damage}{actors}{$dmg_actor}{$spell}{$target}{total} unless $self->{raid}{$target}{class};
             }
         }
     }
@@ -97,12 +134,199 @@ sub page {
         $self->{ext}{Activity}{actors}{$PLAYER}{all}{time}/60, 
         $self->{ext}{Activity}{actors}{$PLAYER}{all}{time}%60, 
         $self->{ext}{Activity}{actors}{$PLAYER}{all}{time}/$ptime*100, 
-        $alldmg/$self->{ext}{Activity}{actors}{$PLAYER}{all}{time} ) 
-            if $ptime && $alldmg && $self->{ext}{Activity}{actors}{$PLAYER} && $self->{ext}{Activity}{actors}{$PLAYER}{all}{time};
+        $dmg_to_mobs/$self->{ext}{Activity}{actors}{$PLAYER}{all}{time} ) 
+            if $ptime && $dmg_to_mobs && $self->{ext}{Activity}{actors}{$PLAYER} && $self->{ext}{Activity}{actors}{$PLAYER}{all}{time};
     
     $PAGE .= $pm->textBox( $presence_text, "Actor Information" );
     
     $PAGE .= "<br />";
+    
+    ##########
+    # DAMAGE #
+    ##########
+    
+    $PAGE .= $pm->tableStart;
+    
+    if( $alldmg ) {
+        my @damageHeader = (
+                "Damaging Ability",
+                "R-Total",
+                "R-Hits",
+                "R-Avg Hit",
+                "R-Crits",
+                "R-Avg Crit",
+                "R-Ticks",
+                "R-Avg Tick",
+                "R-Crit",
+                "R-Crush",
+                "R-Glance",
+                "R-Avoid %",
+                "M/D/P/B/A/R/I",
+            );
+    
+        my @spellsort = sort {
+            $damage_spell{$b}{total} <=> $damage_spell{$a}{total}
+        } keys %damage_spell;
+    
+        $PAGE .= $pm->tableHeader(@damageHeader);
+        foreach my $spellkey (@spellsort) {
+            # $id is for javascript
+            my $id = lc $spellkey;
+            $id =~ s/[^\w]/_/g;
+        
+            # $sdata is totals for the overall spell
+            my $sdata;
+            $sdata = $damage_spell{$spellkey};
+            next unless $sdata->{total};
+            
+            # In case this is an encoded pet spell, split it into $spellactor, $spellid, and $spellname
+            my $spellactor;
+            my $spellid;
+            my $spellname;
+            
+            if( $spellkey =~ /^([A-Za-z0-9]+): (.+)$/ ) {
+                $spellactor = $1;
+                $spellid = $2;
+                $spellname = sprintf( "%s: %s", $pm->actorLink( $1, $self->{ext}{Index}->actorname($1), $pm->classColor( $self->{raid}{$1}{class} ) ), $self->{ext}{Index}->spellname($2) );
+            } else {
+                $spellactor = $PLAYER;
+                $spellid = $spellkey;
+                $spellname = $self->{ext}{Index}->spellname($spellkey);
+            }
+            
+            $PAGE .= $pm->tableRow( 
+                header => \@damageHeader,
+                data => {
+                    "Damaging Ability" => $spellname,
+                    "R-Total" => $sdata->{total},
+                    "R-Hits" => $sdata->{hitCount} && sprintf( "%d", $sdata->{hitCount} ),
+                    "R-Avg Hit" => $sdata->{hitCount} && $sdata->{hitTotal} && sprintf( "%d (%d&ndash;%d)", $sdata->{hitTotal} / $sdata->{hitCount}, $sdata->{hitMin}, $sdata->{hitMax} ),
+                    "R-Ticks" => $sdata->{tickCount} && sprintf( "%d", $sdata->{tickCount} ),
+                    "R-Avg Tick" => $sdata->{tickCount} && $sdata->{tickTotal} && sprintf( "%d (%d&ndash;%d)", $sdata->{tickTotal} / $sdata->{tickCount}, $sdata->{tickMin}, $sdata->{tickMax} ),
+                    "R-Crits" => $sdata->{critCount} && sprintf( "%d", $sdata->{critCount} ),
+                    "R-Avg Crit" => $sdata->{critCount} && $sdata->{critTotal} && sprintf( "%d (%d&ndash;%d)", $sdata->{critTotal} / $sdata->{critCount}, $sdata->{critMin}, $sdata->{critMax} ),
+                    "R-Crit" => ($sdata->{count} - $sdata->{tickCount}) && sprintf( "%0.1f%%", $sdata->{critCount} / ($sdata->{count} - $sdata->{tickCount}) * 100 ),
+                    "R-Glance" => ($sdata->{count} - $sdata->{tickCount}) && sprintf( "%0.1f%%", $sdata->{glancing} / ($sdata->{count} - $sdata->{tickCount}) * 100 ),
+                    "R-Crush" => ($sdata->{count} - $sdata->{tickCount}) && sprintf( "%0.1f%%", $sdata->{crushing} / ($sdata->{count} - $sdata->{tickCount}) * 100 ),
+                    "R-Avoid %" => ($sdata->{count} - $sdata->{tickCount}) && sprintf( "%0.1f%%", ($sdata->{count} - $sdata->{tickCount} - $sdata->{hitCount} - $sdata->{critCount}) / ($sdata->{count} - $sdata->{tickCount}) * 100 ),
+                    "M/D/P/B/A/R/I" => sprintf( "%d/%d/%d/%d/%d/%d/%d", $sdata->{missCount}, $sdata->{dodgeCount}, $sdata->{parryCount}, $sdata->{blockCount}, $sdata->{absorbCount}, $sdata->{resistCount}, $sdata->{immuneCount} ),
+                },
+                type => "master",
+                name => "damage_$id",
+            );
+
+            foreach my $target (sort { $self->{ext}{Damage}{actors}{$spellactor}{$spellid}{$b}{total} <=> $self->{ext}{Damage}{actors}{$spellactor}{$spellid}{$a}{total} } keys %{ $self->{ext}{Damage}{actors}{$spellactor}{$spellid} }) {
+                # Reassign $sdata to the per-target breakdown
+                $sdata = $self->{ext}{Damage}{actors}{$spellactor}{$spellid}{$target};
+                next unless $sdata->{total};
+            
+                $PAGE .= $pm->tableRow( 
+                    header => \@damageHeader,
+                    data => {
+                        "Damaging Ability" => $pm->actorLink( $target, $self->{ext}{Index}->actorname($target), $pm->classColor( $self->{raid}{$target}{class} ) ),
+                        "R-Total" => $sdata->{total},
+                        "R-Hits" => $sdata->{hitCount} && sprintf( "%d", $sdata->{hitCount} ),
+                        "R-Avg Hit" => $sdata->{hitCount} && $sdata->{hitTotal} && sprintf( "%d (%d&ndash;%d)", $sdata->{hitTotal} / $sdata->{hitCount}, $sdata->{hitMin}, $sdata->{hitMax} ),
+                        "R-Ticks" => $sdata->{tickCount} && sprintf( "%d", $sdata->{tickCount} ),
+                        "R-Avg Tick" => $sdata->{tickCount} && $sdata->{tickTotal} && sprintf( "%d (%d&ndash;%d)", $sdata->{tickTotal} / $sdata->{tickCount}, $sdata->{tickMin}, $sdata->{tickMax} ),
+                        "R-Crits" => $sdata->{critCount} && sprintf( "%d", $sdata->{critCount} ),
+                        "R-Avg Crit" => $sdata->{critCount} && $sdata->{critTotal} && sprintf( "%d (%d&ndash;%d)", $sdata->{critTotal} / $sdata->{critCount}, $sdata->{critMin}, $sdata->{critMax} ),
+                        "R-Crit" => ($sdata->{count} - $sdata->{tickCount}) && sprintf( "%0.1f%%", $sdata->{critCount} / ($sdata->{count} - $sdata->{tickCount}) * 100 ),
+                        "R-Glance" => ($sdata->{count} - $sdata->{tickCount}) && sprintf( "%0.1f%%", $sdata->{glancing} / ($sdata->{count} - $sdata->{tickCount}) * 100 ),
+                        "R-Crush" => ($sdata->{count} - $sdata->{tickCount}) && sprintf( "%0.1f%%", $sdata->{crushing} / ($sdata->{count} - $sdata->{tickCount}) * 100 ),
+                        "R-Avoid %" => sprintf( "%0.1f%%", ($sdata->{count} - $sdata->{tickCount}) && ($sdata->{count} - $sdata->{tickCount} - $sdata->{hitCount} - $sdata->{critCount}) / ($sdata->{count} - $sdata->{tickCount}) * 100 ),
+                        "M/D/P/B/A/R/I" => sprintf( "%d/%d/%d/%d/%d/%d/%d", $sdata->{missCount}, $sdata->{dodgeCount}, $sdata->{parryCount}, $sdata->{blockCount}, $sdata->{absorbCount}, $sdata->{resistCount}, $sdata->{immuneCount} ),
+                    },
+                    type => "slave",
+                    name => "damage_$id",
+                );
+            }
+
+            $PAGE .= $pm->jsClose("damage_$id");
+        }
+    
+    }
+    
+    ###########
+    # HEALING #
+    ###########
+    
+    # if( keys %healtot ) {
+    # 
+    #     my @healingHeader = (
+    #             "Healing Ability",
+    #             "R-Eff. Heal",
+    #             "R-Hits",
+    #             "R-Avg Hit",
+    #             "R-Crits",
+    #             "R-Avg Crit",
+    #             "R-Ticks",
+    #             "R-Avg Tick",
+    #             "R-Crit %",
+    #             "R-Overheal %",
+    #             "",
+    #             "",
+    #             "",
+    #         );
+    # 
+    #     my @spellnames = sort {
+    #         $healtot{$b}{effective} <=> $healtot{$a}{effective}
+    #     } keys %healtot;
+    # 
+    #     $PAGE .= $pm->tableHeader(@healingHeader);
+    #     foreach my $spellname (@spellnames) {
+    #         my $id = lc $spellname;
+    #         $id =~ s/[^\w]/_/g;
+    # 
+    #         my $sdata;
+    #         $sdata = $healtot{$spellname};
+    #         $PAGE .= $pm->tableRow( 
+    #             header => \@healingHeader,
+    #             data => {
+    #                 "Healing Ability" => $spellname,
+    #                 "R-Eff. Heal" => $sdata->{effective},
+    #                 "R-Overheal %" => $sdata->{total} ? sprintf "%0.1f%%", ($sdata->{total} - $sdata->{effective} ) / $sdata->{total} * 100 : "",
+    #                 "R-Hits" => $sdata->{hitCount} ? sprintf "%d", $sdata->{hitCount} : "",
+    #                 "R-Avg Hit" => $sdata->{hitCount} ? sprintf "%d", $sdata->{hitTotal} / $sdata->{hitCount} : "",
+    #                 "R-Ticks" => $sdata->{tickCount} ? sprintf "%d", $sdata->{tickCount} : "",
+    #                 "R-Avg Tick" => $sdata->{tickCount} ? sprintf "%d", $sdata->{tickTotal} / $sdata->{tickCount} : "",
+    #                 "R-Crits" => $sdata->{critCount} ? sprintf "%d", $sdata->{critCount} : "",
+    #                 "R-Avg Crit" => $sdata->{critCount} ? sprintf "%d", $sdata->{critTotal} / $sdata->{critCount} : "",
+    #                 "R-Crit %" => $sdata->{count} - $sdata->{tickCount} > 0 ? sprintf "%0.1f%%", $sdata->{critCount} / ($sdata->{count} - $sdata->{tickCount}) * 100 : "",
+    #             },
+    #             type => "master",
+    #             name => "healing_$id",
+    #         );
+    # 
+    #         foreach my $target (sort { $actors{$PLAYER}{healing}{targets}{$b}{spells}{$spellname}{effective} <=> $actors{$PLAYER}{healing}{targets}{$a}{spells}{$spellname}{effective} } keys %{ $actors{$PLAYER}{healing}{targets} }) {
+    #             $sdata = $actors{$PLAYER}{healing}{targets}{$target}{spells}{$spellname};
+    #             next unless $sdata->{total};
+    # 
+    #             $PAGE .= $pm->tableRow( 
+    #                 header => \@healingHeader,
+    #                 data => {
+    #                     "Healing Ability" => $pm->actorLink( $target, $pm->classColor( $raid{$target}{class} ) ),
+    #                     "R-Eff. Heal" => $sdata->{effective},
+    #                     "R-Overheal %" => $sdata->{total} ? sprintf "%0.1f%%", ($sdata->{total} - $sdata->{effective} ) / $sdata->{total} * 100 : "",
+    #                     "R-Hits" => $sdata->{hitCount} ? sprintf "%d", $sdata->{hitCount} : "",
+    #                     "R-Avg Hit" => $sdata->{hitCount} ? sprintf "%d", $sdata->{hitTotal} / $sdata->{hitCount} : "",
+    #                     "R-Ticks" => $sdata->{tickCount} ? sprintf "%d", $sdata->{tickCount} : "",
+    #                     "R-Avg Tick" => $sdata->{tickCount} ? sprintf "%d", $sdata->{tickTotal} / $sdata->{tickCount} : "",
+    #                     "R-Crits" => $sdata->{critCount} ? sprintf "%d", $sdata->{critCount} : "",
+    #                     "R-Avg Crit" => $sdata->{critCount} ? sprintf "%d", $sdata->{critTotal} / $sdata->{critCount} : "",
+    #                     "R-Crit %" => $sdata->{count} - $sdata->{tickCount} > 0 ? sprintf "%0.1f%%", $sdata->{critCount} / ($sdata->{count} - $sdata->{tickCount}) * 100 : "",
+    #                 },
+    #                 type => "slave",
+    #                 name => "healing_$id",
+    #             );
+    #         }
+    # 
+    #         $PAGE .= $pm->jsClose("healing_$id");
+    #     }
+    # }
+    
+    $PAGE .= $pm->tableEnd;
+    
     
     ##########
     # FOOTER #
